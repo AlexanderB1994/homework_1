0e9ad98bea79f30830cdab3d1b77f249c6b1e727
AlexanderB1994 <bulgaru.alexander1994@gmail.com>
2023-03-04 20:44:22 +0200

    update homework1

--- homework.py
+++ homework.py
@@ -1,12 +1,12 @@
 my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+my_list = sorted(my_list)
 # my ascending list
-my_list.sort()
 print(my_list)
 # my descending list
 print(sorted(my_list, reverse=True))
 # even numbers
 print(my_list[1::2])
 # odd numbers
-print(my_list[0::2])
+print(my_list[::2])
 # the multiples of 3
 print(my_list[2::3])
